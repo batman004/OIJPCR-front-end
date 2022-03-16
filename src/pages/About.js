9a9d49a4b86e4f2378526daf7d2e5f5464e40f9b
Kabir Singh Shekhawat <kabirsinghshekhawat1789@gmail.com>
2022-03-16 12:23:29 +0530

    draft: team cards

--- src/pages/About.js
+++ src/pages/About.js
@@ -11,6 +11,8 @@ const About = () => (
         {HeadingText}
       </Heading>
 
+      <EditorialBoardMembers />
+
       <Description>
         {AboutText}
       </Description>
@@ -46,5 +48,60 @@ function Description(props) {
     </p>
   )
 }
+
+
+const boardMembers = [
+  {
+    name: "Prof Deepsheekha Kotwal",
+    university: "Central University of Jammu",
+    email: "kotwaldeepsheekha@gmail.com"
+  },
+  {
+    name: "Dr.Sudha Khokate",
+    university: "Associate Professor, Dept of Sociology, Bangalore University",
+    email: "drsudhakhokate@gmail.com"
+  },
+  {
+    name: "Dr.Shruti Singh",
+    university: "Associate Professor, Amity Institute of Social Sciences(AISS)",
+    email: "ssingh31@amity.edu"
+  },
+  {
+    name: "Prof Sangeeta Thapliyal",
+    university: "Centre for Inner Asian Studies, Jawaharlal Nehru University",
+    email: "sangeetathapliyal@gmail.com"
+  },
+  {
+    name: "Dr Mallika Joseph",
+    university: "Senior Fellow, Women in Security, Conflict Management and Peace(WISCOMP), India Habitat Centre",
+    email: "mallikajoseph@gmail.com"
+  },
+]
+
+function EditorialBoardMembers() {
+  return (
+    <>
+      <div className='mt-6 mb-4'>
+        <h2 className='text-xl font-bold text-black md:text-2xl'>
+          Editorial Board Members
+        </h2>
+      </div>
+      <div className='grid grid-cols-1 gap-4 md:grid-cols-2'>
+        {
+          boardMembers.map((member, index) => {
+            return (
+              <div className='px-2 py-4 mx-4 my-6 rounded-md shadow-lg lg:h-48'>
+                <h3 className='my-2 text-xl font-medium text-gray-900 md:text-2xl'>{member.name}</h3>
+                <p className='my-2 font-normal text-gray-800 text-md md:text-lg'>{member.university}</p>
+                <p className='text-sm font-light text-gray-700 md:text-md'>Email: {member.email}</p>
+              </div>
+            )
+          })
+        }
+      </div>
+    </>
+  )
+}
+
 
 export default About
