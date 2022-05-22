ebf57740314bfc1f9c5ed04b3ce0aac3c3f52e59
Yuvraj Singh Pathania <58564635+batman004@users.noreply.github.com>
2022-05-22 13:13:46 -0700

    fix : copyright year range + href links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -24,11 +24,11 @@ function CopyrightLeftPanel({ logo }) {
   return (
     <div className="flex-1 w-full py-2 pl-4 pr-2 md:w-1/2 md:p-2 noprint">
       <img src={logo} alt="OIJPCR logo" className="w-14 h-14 md:w-20 md:h-20" />
-      <p className="mt-2 md:mt-6">&copy; Copyright 2016-2021</p>
+      <p className="mt-2 md:mt-6">&copy; Copyright 2016-2022</p>
       <p className='mb-4'>Online Indian Journal of Peace and Conflict Resolution</p>
       <p>Developed and Maintained by
-        <a href='kabirsingh.super.site' className='underline text-gray-50 hover:cursor-pointer'> Kabir Singh Shekhawat</a> and
-        <a href="yuvraj.super.site" className='underline text-gray-50 hover:cursor-pointer'> Yuvraj Singh Pathania</a>
+        <a href='https://kabirsingh.super.site' className='underline text-gray-50 hover:cursor-pointer'> Kabir Singh Shekhawat</a> and
+        <a href="https://yuvraj.super.site" className='underline text-gray-50 hover:cursor-pointer'> Yuvraj Singh Pathania</a>
       </p>
     </div>
   )
