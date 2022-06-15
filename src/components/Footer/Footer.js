e5795ea3efc470f9212ec2b621889ec7438ffd68
batman004 <pathania.yuvraj@gmail.com>
2022-06-16 00:28:30 +0530

    add : address in footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -25,7 +25,10 @@ function CopyrightLeftPanel({ logo }) {
     <div className="flex-1 w-full py-2 pl-4 pr-2 md:w-1/2 md:p-2 noprint">
       <img src={logo} alt="OIJPCR logo" className="w-14 h-14 md:w-20 md:h-20" />
       <p className="mt-2 md:mt-6">&copy; Copyright 2016-2022</p>
-      <p className='mb-4'>Online Indian Journal of Peace and Conflict Resolution</p>
+      <p className='mb-4'>Online Indian Journal of Peace and Conflict Resolution
+      <br></br>
+      Sector-37, Noida, UP, India - 201303
+      </p>
       <p>Developed and Maintained by
         <a href='https://kabirsingh.super.site' className='underline text-gray-50 hover:cursor-pointer'> Kabir Singh Shekhawat</a> and
         <a href="https://yuvraj.super.site" className='underline text-gray-50 hover:cursor-pointer'> Yuvraj Singh Pathania</a>
