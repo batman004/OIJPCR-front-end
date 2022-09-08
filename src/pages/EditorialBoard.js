b768a55b14baa60e78b385b5304ad798967e8ecc
KabirSinghShekhawat <kabirsinghshekhawat1789@gmail.com>
2022-09-08 19:33:11 +0530

    add: fix guidelines

--- src/pages/EditorialBoard.js
+++ src/pages/EditorialBoard.js
@@ -2,120 +2,107 @@ import Podcast from '../components/Home/Podcast/Podcast'
 
 const EditorialBoard = () => (
     <div className="flex-grow max-w-7xl">
-
-        <EditorialBoardMembers />
-  
-        <div className="sm:mx-4">
-          <Podcast />
+        <div className="mx-2 sm:mx-4">
+            <EditorialBoardMembers/>
         </div>
-    </div>
-  )
-const boardMembers = [
-  {
+
+        <div className="sm:mx-4">
+            <Podcast/>
+        </div>
+    </div>)
+const boardMembers = [{
     name: "Dr. Sudha Khokate",
     designation: "Assistant Professor",
     university: "Department of Sociology, Manasa Bhavana Jnanabharathi Campus, Bangalore University",
     email: "drsudhakhokate@bub.ernet.in",
     website: "http://eng.bangaloreuniversity.ac.in"
-  },
-  {
+}, {
     name: "Dr. Shruti Singh",
     designation: "Professor",
     university: "Amity Institute of Social Sciences (AISS), Noida",
     email: "ssingh@amity.edu",
     website: "https://www.amity.edu/faculty-detail.aspx?facultyID=3246"
-  },
-  {
+}, {
     name: "Prof Sangeeta Thapliyal",
-    designation:"Professor & Head Center for Inner Asian Studies",
+    designation: "Professor & Head Center for Inner Asian Studies",
     university: "Centre for Inner Asian Studies, Jawaharlal Nehru University",
     email: "sthapliyal@mail.jnu.ac.in",
     website: "https://jnu.ac.in/Faculty/sthapliyal"
-  },
-  {
+}, {
     name: "Dr. Nagalaxmi M Raman",
-    designation:"Professor & Director",
+    designation: "Professor & Director",
     university: "Amity Institute of International Studies (AIIS), Noida",
     email: "niraman@amity.edu",
     website: "https://www.amity.edu/faculty-detail.aspx?facultyID=1530"
-  },
-  {
+}, {
     name: "Dr. Mallika Joseph",
     designation: "Adjunct Professor",
     university: "National Institute of Advanced Studies Indian Institute of science Campus,Bengaluru",
     email: "mallikajoseph@nias.res.in",
     website: "https://www.nias.res.in/People/facility-member-info/People/Mallika%20Joseph%20A"
-  },
-  {
+}, {
     name: "Rashmi Thakur",
     designation: "PGT Political Science",
     university: "Mayo College Girls School, Mayo Link Road",
     email: "rthakur@mcgs.ac.in",
     website: "https://www.mcgs.ac.in/our_team/rashmi-thakur/"
-  },
-  {
+}, {
     name: "Prof Deepsheekha Kotwal",
-    designation:"Professor",
+    designation: "Professor",
     university: "Central University of Jammu",
     email: "d.kotwal@oijpcr.org"
-  },
-  {
+}, {
     name: "Shomaila Tahzib",
-    designation:"Social activist, research scholar and gold Medalist",
+    designation: "Social activist, research scholar and gold Medalist",
     university: "St.Xavier’s College, Ranchi",
     email: "s.tahzib@oijpcr.org"
-  },
-]
+},]
 
 function EditorialBoardMembers() {
-  return (
-    <div className='mt-6 mb-8 lg:mt-12 lg:mb-16'>
-      <div className='mb-4 lg:mb-8'>
-        <h2 className='text-xl font-bold text-black md:text-2xl lg:text-3xl'>
-          Editorial Board Members
-        </h2>
-      </div>
-      <div className='grid grid-cols-1 gap-4 md:grid-cols-2 grid-flow-auto md:grid-flow-row'>
-        {
-          boardMembers.map((member, index) => {
-            return (
-              <BoardMemberCard
-                key={index}
-                name={member.name}
-                designation={member.designation}
-                university={member.university}
-                email={member.email}
-                website={member.website}
-              />
-            )
-          })
-        }
-      </div>
-    </div>
-  )
+    return (<div className='mt-6 mb-8 lg:mt-12 lg:mb-16'>
+        <div className='mb-4 lg:mb-8'>
+            <h2 className='text-center sm:text-left text-xl font-bold text-black md:text-2xl lg:text-3xl'>
+                Editorial Board Members
+            </h2>
+        </div>
+        <div
+            className='grid grid-cols-1 gap-4 md:grid-cols-2 grid-flow-auto md:grid-flow-row'>
+            {boardMembers.map((member, index) => {
+                return (<BoardMemberCard
+                    key={index}
+                    name={member.name}
+                    designation={member.designation}
+                    university={member.university}
+                    email={member.email}
+                    website={member.website}
+                />)
+            })}
+        </div>
+    </div>)
 }
 
 
 function BoardMemberCard({
-  name,
-  designation,
-  university,
-  email,
-  website
-}) {
-  return (
-    <div className='grid grid-cols-1 gap-2 p-4 rounded-md shadow-lg lg:gap-4'>
-      <h3 className='text-xl font-medium text-black md:text-2xl'>{name}</h3>
-      <p className='font-normal text-gray-900 text-md md:text-lg'>{designation}</p>
-      <p className='font-normal text-gray-900 text-md md:text-lg'>{university}</p>
-      <p className='text-sm font-light text-gray-800 md:text-md'>
-        <a href={`mailto:${email}`} className='hover:text-blue-800 hover:underline'>{email}</a>
-      </p>
-      <p className='text-sm font-light text-gray-800 md:text-md'>
-        <a href={`${website}`} className='hover:text-blue-800 hover:underline'>website</a>
-      </p>
-    </div>
-  )
+                             name,
+                             designation,
+                             university,
+                             email,
+                             website
+                         }) {
+    return (<div
+        className='grid grid-cols-1 gap-2 p-4 rounded-md shadow-lg lg:gap-4'>
+        <h3 className='text-xl font-medium text-black md:text-2xl'>{name}</h3>
+        <p className='font-normal text-gray-900 text-md md:text-lg'>{designation}</p>
+        <p className='font-normal text-gray-900 text-md md:text-lg'>{university}</p>
+        <p className='text-sm font-light text-gray-800 md:text-md'>
+            <a href={`mailto:${email}`}
+               className='hover:text-blue-800 hover:underline'>{email}</a>
+        </p>
+        <p className='text-sm font-light text-gray-800 md:text-md'>
+            <a href={`${website}`}
+               className='hover:text-blue-800 hover:underline'>website</a>
+        </p>
+    </div>)
 }
 
 export default EditorialBoard
--- src/pages/SubmitArticle.js
+++ src/pages/SubmitArticle.js
@@ -1,46 +1,30 @@
 const guidelinesList = [
-  {guidelines: 'The topics for submission should be related to peace and conflict resolution, peacebuilding activities at the economic, political, civil society and the social-cultural levels, with specific reference to India.', key: 1},
-  {guidelines: 'The articles are published quarterly online, however open to print as well.', key: 2},
-  {guidelines: 'OIJPCR welcomes concise, pointed, articles with few recommendations.', key: 3},
-  {guidelines: 'All articles to be between 1000 -1500 words Helvetica Neue (bold), 16 size font with 1.5 Line spacing.', key: 4},
-  {guidelines: 'A bibliography is not compulsory if it is a commentary or opinionated piece. But in case of a research article then Chicago style manual style is to be followed.', key: 5},
-  {guidelines: 'All references if used should be mentioned within the article itself.', key: 6},
-  {guidelines: 'Articles for submission should be exclusively for OIJPCR.', key: 7},
-  {guidelines: 'The contributor will be responsible for factual accuracy and research integrity.', key: 8},
-  {guidelines: 'Article email and short brief not exceeding 30 words, should be included along with the article.', key: 9},
-  {guidelines: 'All articles to be emailed to Editor at editor@oijpcr.org', key: 10},
-  {guidelines: 'All contributions will be reviewed and edited and the decision of the Editorial team would be final.', key: 11},
-  {guidelines: 'We also welcome Book reviews of 1000-1500 words with a photo of the book as well, but the books to be reviewed should be related to Peace & Conflict resolution Studies only.', key: 12},
-  {guidelines: 'Disclaimer: Opinions in the articles may not necessarily coincide with those of the Editorial Team.', key: 13},
+    "The topics for submission should be related to peace and conflict resolution, peacebuilding activities at the economic, political, civil society and the social-cultural levels, with specific reference to India.", "The articles are published quarterly online, however open to print as well.", "OIJPCR welcomes concise, pointed, articles with few recommendations.", "All articles to be between 1000 -1500 words Helvetica Neue (bold), 16 size font with 1.5 Line spacing.", "A bibliography is not compulsory if it is a commentary or opinionated piece. But in case of a research article then Chicago style manual style is to be followed.", "All references if used should be mentioned within the article itself.", "Articles for submission should be exclusively for OIJPCR.", "The contributor will be responsible for factual accuracy and research integrity.", "Article email and short brief not exceeding 30 words, should be included along with the article.", "All articles to be emailed to Editor at editor@oijpcr.org", "All contributions will be reviewed and edited and the decision of the Editorial team would be final.", "We also welcome Book reviews of 1000-1500 words with a photo of the book as well, but the books to be reviewed should be related to Peace & Conflict resolution Studies only.", "Disclaimer: Opinions in the articles may not necessarily coincide with those of the Editorial Team."
 ];
 
-const SubmitArticle = () => (
-  <div className="flex-grow">
-    <div className="flex items-center justify-center my-6 md:my-4">
-      <h1 className="text-4xl">Guidelines for submissions</h1>
+const SubmitArticle = () => (<div className="flex-grow">
+    <div className="flex items-center justify-center my-4">
+        <h1 className="text-2xl sm:text-3xl md:text-4xl">Guidelines
+            for submissions</h1>
     </div>
-    <div className="indent-8">
-      <ol>
-        {guidelinesList.map(guidelines =>
-          <li key={guidelines.key}>{guidelines.guidelines}</li>
-        )}
-      </ol>
+    <div className="mx-4 my-2">
+        <ol className="list-decimal py-2 px-4 md:px-8">
+            {guidelinesList.map((guideline, index) => {
+                return (<li key={index}>{guideline}</li>);
+            })}
+        </ol>
     </div>
-    <div className="flex items-center justify-center mt-6">
-      <iframe
-        src="https://docs.google.com/forms/d/e/1FAIpQLSdXrEyabD4y9YcYriSA9V80UT1zYdQ2pyHJbPW8P9B7ekwCrA/viewform?embedded=true"
-        title="Submit Article"
-        width={640}
-        height={968}
-        frameBorder={0}
-        marginHeight={0}
-        marginWidth={0}
-      >
-        Loading…
-      </iframe>
+    <div className="grid place-items-center">
+        <iframe
+            src="https://docs.google.com/forms/d/e/1FAIpQLSdXrEyabD4y9YcYriSA9V80UT1zYdQ2pyHJbPW8P9B7ekwCrA/viewform?embedded=true"
+            title="Submit Article"
+            className="w-full"
+            height={1024}
+        >
+            Loading…
+        </iframe>
     </div>
-  </div>
-)
+</div>);
 
 
-export default SubmitArticle
+export default SubmitArticle;
