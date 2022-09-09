841553c52c13f88e94c783432f594d3bf16d1157
KabirSinghShekhawat <kabirsinghshekhawat1789@gmail.com>
2022-09-10 00:34:56 +0530

    fix: guidelines

--- src/pages/SubmitArticle.js
+++ src/pages/SubmitArticle.js
@@ -2,15 +2,15 @@ const guidelinesList = [
     "The topics for submission should be related to peace and conflict resolution, peacebuilding activities at the economic, political, civil society and the social-cultural levels, with specific reference to India.", "The articles are published quarterly online, however open to print as well.", "OIJPCR welcomes concise, pointed, articles with few recommendations.", "All articles to be between 1000 -1500 words Helvetica Neue (bold), 16 size font with 1.5 Line spacing.", "A bibliography is not compulsory if it is a commentary or opinionated piece. But in case of a research article then Chicago style manual style is to be followed.", "All references if used should be mentioned within the article itself.", "Articles for submission should be exclusively for OIJPCR.", "The contributor will be responsible for factual accuracy and research integrity.", "Article email and short brief not exceeding 30 words, should be included along with the article.", "All articles to be emailed to Editor at editor@oijpcr.org", "All contributions will be reviewed and edited and the decision of the Editorial team would be final.", "We also welcome Book reviews of 1000-1500 words with a photo of the book as well, but the books to be reviewed should be related to Peace & Conflict resolution Studies only.", "Disclaimer: Opinions in the articles may not necessarily coincide with those of the Editorial Team."
 ];
 
-const SubmitArticle = () => (<div className="flex-grow">
+const SubmitArticle = () => (<div className="flex-grow max-w-7xl">
     <div className="flex items-center justify-center my-4">
         <h1 className="text-2xl sm:text-3xl md:text-4xl">Guidelines
             for submissions</h1>
     </div>
-    <div className="mx-4 my-2">
+    <div className="mx-4 my-2 max-w-5xl">
         <ol className="list-decimal py-2 px-4 md:px-8">
             {guidelinesList.map((guideline, index) => {
-                return (<li key={index}>{guideline}</li>);
+                return (<li key={index} className="py-2 font-medium font-sans">{guideline}</li>);
             })}
         </ol>
     </div>
