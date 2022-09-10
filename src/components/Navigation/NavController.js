0b878979dcd78b25d563e6d5ea65bb70db96ecc8
KabirSinghShekhawat <kabirsinghshekhawat1789@gmail.com>
2022-09-10 13:51:01 +0530

    add: Contact Page

--- src/components/Navigation/NavController.js
+++ src/components/Navigation/NavController.js
@@ -1,7 +1,8 @@
-import { Redirect, Route, Switch } from 'react-router-dom'
+import {Redirect, Route, Switch} from 'react-router-dom'
 import Home from '../../pages/Home'
 import About from '../../pages/About'
 import EditorialBoard from '../../pages/EditorialBoard'
+import Contact from '../../pages/Contact'
 import Archive from '../../pages/Archive/Archive'
 import SubmitArticle from '../../pages/SubmitArticle'
 import React from 'react'
@@ -10,34 +11,36 @@ import Footer from '../Footer/Footer'
 import Tag from '../../pages/Topic/Tag'
 
 const NavController = () => {
-  return (
-    <div className="flex flex-col items-center h-screen">
-      <Nav/>
-      <Switch>
+    return (
+        <div className="flex flex-col items-center h-screen">
+            <Nav/>
+            <Switch>
 
-        <Route exact path="/about" render={() => <About/>}/>
+                <Route exact path="/about" render={() => <About/>}/>
 
-        <Route exact path="/editorialBoard" render={() => <EditorialBoard/>}/>
+                <Route exact path="/editorialBoard" render={() => <EditorialBoard/>}/>
 
-        <Route exact path="/tags/:tag" render={(props) => <Tag {...props}/>}/>
+                <Route exact path="/contact" render={() => <Contact/>}/>
 
-        <Route path="/archive"
-               render={(props) => <Archive {...props}/>}
-        />
+                <Route exact path="/tags/:tag" render={(props) => <Tag {...props}/>}/>
 
-        <Route exact path="/submitArticle"
-               render={() => <SubmitArticle/>}
-        />
+                <Route path="/archive"
+                       render={(props) => <Archive {...props}/>}
+                />
 
-        <Route exact path="/" render={() => <Home/>}/>
+                <Route exact path="/submitArticle"
+                       render={() => <SubmitArticle/>}
+                />
 
-        <Route path="*" >
-          <Redirect to="/notFound"/>
-        </Route>
-      </Switch>
-      <Footer/>
-    </div>
-  )
+                <Route exact path="/" render={() => <Home/>}/>
+
+                <Route path="*">
+                    <Redirect to="/notFound"/>
+                </Route>
+            </Switch>
+            <Footer/>
+        </div>
+    )
 }
 
 export default NavController
