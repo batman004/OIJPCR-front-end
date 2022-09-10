8b1bdb2fca271694aec782e11ea9f1031c0764c6
batman004 <pathania.yuvraj@gmail.com>
2022-09-10 13:52:35 +0530

    fix : journal details

--- src/components/About/Description.js
+++ src/components/About/Description.js
@@ -14,8 +14,13 @@ const AboutText = () => {
                 <span>
                         It is an irony that ‘India – the Land of Ahimsa and Peace’, has very few centre pursuing Peace Studies and promoting the development of skills for conflict resolution.
                         ‘Online Indian Journal of Peace and Conflict Resolution’ is a novel initiative started by
-                        <Author />
-                        to study, examine and analyse the multifarious methods, mechanisms and theories of conflict resolution used by organizations, institutions, social activists, academicians, individuals and practitioners. This journal will collate successful models and practices which could be referred to or tried out in similar cases/areas of dispute/conflict, thus positively contributing and helping in the creation of a peaceful society and Nation. The Journal shall be a quarterly feature and solicits contribution of commentaries on factual cases as well as articles of academic or theoretical nature on the subject, which will be reviewed and published for understanding and dealing with conflict situations and furtherance of peace studies.
+                        <Author /> 
+                        in March 2016, to study, examine and analyse the multifarious methods, mechanisms and theories of conflict resolution used by organizations, institutions, social activists, academicians, individuals and practitioners. 
+                        <br></br>
+                        <br></br>
+                        <b>Aim and Scope</b> 
+                        <br></br>
+                        This journal will collate successful models and practices which could be referred to or tried out in similar cases/areas of dispute/conflict, thus positively contributing and helping in the creation of a peaceful society and Nation. The Journal shall be an annual feature and solicits contribution of commentaries on factual cases as well as articles of academic or theoretical nature on the subject, which will be reviewed and published for understanding and dealing with conflict situations and furtherance of peace studies.
                 </span >
         )
 }
