5680c2791c01e12f5115b2e0cda3a7cae2c80365
KabirSinghShekhawat <kabirsinghshekhawat1789@gmail.com>
2023-02-28 19:04:23 +0530

    format: left grid

--- src/components/Home/LeftGrid.js
+++ src/components/Home/LeftGrid.js
@@ -1,78 +1,75 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios'
+import { useEffect, useState } from "react";
+import axios from "axios";
 import { LoadingCard, LoadingCardFullWidth } from "../Loaders";
 import { ArticleCard, ArticleCardFullWidth } from "../Cards";
-import config from '../../config/config'
+import config from "../../config/config";
 
 function LeftGrid() {
-  const [cardData, setCardData] = useState([1, 2, 3, 4, 5])
+  const [cardData, setCardData] = useState([1, 2, 3, 4, 5]);
 
   useEffect(() => {
     const getCardData = async () => {
-      const journals = await fetchJournals()
-      let newCardData = cardData.map(data => {
-        const journal = journals.shift()
-        return journal || data
-      })
-      setCardData(newCardData)
-    }
-    console.log("left grid render");
-    getCardData()
+      const journals = await fetchJournals();
+      let newCardData = cardData.map((data) => {
+        const journal = journals.shift();
+        return journal || data;
+      });
+      setCardData(newCardData);
+    };
+    getCardData();
     // eslint-disable-next-line
-  }, [])
+  }, []);
 
   const fetchJournals = async () => {
-    const res = await axios.get(`${config.host}/journals/home/5`)
-    return res?.data
-  }
+    const res = await axios.get(`${config.host}/journals/home/5`);
+    return res?.data;
+  };
 
   const cnameFullWidthCard = {
-    container: '',
-    button: 'ml-4 mt-8',
-  }
+    container: "",
+    button: "ml-4 mt-8",
+  };
 
   const cname = {
-    container: '',
-    button: 'ml-4 mt-8',
-  }
+    container: "",
+    button: "ml-4 mt-8",
+  };
 
   const cardGrid = cardData?.map((card, index) => {
     if (!card?.author) {
-      return (
-        index === 0 ?
-          <LoadingCardFullWidth key={index} />
-          :
-          <LoadingCard key={index} />
-      )
+      return index === 0 ? (
+        <LoadingCardFullWidth key={index} />
+      ) : (
+        <LoadingCard key={index} />
+      );
     }
 
-    return (
-      index === 0 ?
-        <ArticleCardFullWidth
-          key={card._id}
-          {...card}
-          coverPhoto={card.cover}
-          id={card._id}
-          volume={card.volume}
-          cname={cnameFullWidthCard}
-        />
-        :
-        <ArticleCard
-          key={card._id}
-          {...card}
-          coverPhoto={card.cover}
-          id={card._id}
-          volume={card.volume}
-          cname={cname}
-        />
-    )
-  })
+    return index === 0 ? (
+      <ArticleCardFullWidth
+        key={card._id}
+        {...card}
+        coverPhoto={card.cover}
+        id={card._id}
+        volume={card.volume}
+        cname={cnameFullWidthCard}
+      />
+    ) : (
+      <ArticleCard
+        key={card._id}
+        {...card}
+        coverPhoto={card.cover}
+        id={card._id}
+        volume={card.volume}
+        cname={cname}
+      />
+    );
+  });
 
   return (
     <div className="grid w-full grid-flow-row grid-cols-1 grid-rows-5 lg:grid-cols-2 lg:grid-rows-3 lg:w-3/4 md:w-2/3">
       {cardGrid}
     </div>
-  )
+  );
 }
 
-export default LeftGrid
+export default LeftGrid;
