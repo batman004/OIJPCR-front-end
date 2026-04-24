e94c06e2efa7d7f28e3c66601dd57bde90acea57
batman004 <pathania.yuvraj@gmail.com>
2026-04-25 00:34:04 +0200

    chore: upgrade package dependencies and update Tailwind CSS configuration for improved compatibility

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,7 @@ module.exports = {
   corePlugins: {
     preflight: true,
   },
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
     extend: {
       colors: {
