001b463c8de95bba29b1a501eb8ecd74f70d573d
batman004 <pathania.yuvraj@gmail.com>
2026-04-24 17:10:25 +0200

    style: update CardButton layout and icon size for improved UI consistency

--- src/components/Cards/CardButton.js
+++ src/components/Cards/CardButton.js
@@ -12,9 +12,9 @@ const CardButton = ({
     <div className={cname}>
       <Link
         to={`/archive/${slug}/${id}`}
-        className="px-4 py-2.5 my-4 mr-4 text-white bg-black rounded-lg sm:my-2 max-w-max"
+        className="inline-flex items-center px-4 py-2.5 my-4 mr-4 text-white bg-black rounded-lg sm:my-2 max-w-max"
       >
-        <img src={alertCircle} className="inline mb-1 mr-2" alt="alert icon" />
+        <img src={alertCircle} className="w-4 h-4 mr-2" alt="alert icon" />
         Read More
       </Link>
       {
@@ -22,9 +22,9 @@ const CardButton = ({
         &&
         <Link
           to={`${pathUrl}/${slug}/${id}`}
-          className="px-4 py-2 my-4 text-white bg-black rounded-lg sm:my-4 max-w-max"
+          className="inline-flex items-center px-4 py-2.5 my-4 mr-4 text-white bg-black rounded-lg sm:my-2 max-w-max"
         >
-          <img src={alertCircle} className="inline mb-1 mr-2" alt="alert icon" />
+          <img src={alertCircle} className="w-4 h-4 mr-2" alt="alert icon" />
           Edit
         </Link>
       }
